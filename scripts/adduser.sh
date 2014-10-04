cb0750a78f4d0a0ac75fdf8b503e71fb65959bd0
Michiel de Jong <michiel@unhosted.org>
2014-10-04 10:41:22 +0100

    First draft for adduser script

---
+++ scripts/adduser.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eux
+
+# Start wordpress service for new site (and create the user)
+systemctl enable wordpress@{$1}.service
+systemctl start  wordpress@{$1}.service
+
+# Configure new site in HAproxy
+IP=`docker inspect --format '{{.NetworkSettings.IPAddress}}' wordpress-$1`
+
+sed s/%HOSTNAME%/$1/ /data/infrastructure/templates/haproxy-site.cfg | sed s/%IP%/$IP/ >> /data/server-wide/haproxy/haproxy.cfg
+systemctl reload haproxy.service
