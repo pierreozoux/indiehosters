b2dbfee3d8d158fcddfda4f15b2dbc481cae12e4
Michiel de Jong <michiel@unhosted.org>
2014-10-04 21:01:32 +0000

    wait for container to start and fix index.html location

--- importers/nginx.sh
+++ importers/nginx.sh
@@ -3,6 +3,6 @@
 if [ ! -d "/data/per-user/$USER/nginx/data" ]; then
   mkdir -p /data/per-user/$USER/nginx
   cd /data/per-user/$USER/nginx
-  echo Hello $USER > index.html
+  echo Hello $USER > data/www-content/index.html
   touch /data/per-user/$USER/nginx/.env
 fi
