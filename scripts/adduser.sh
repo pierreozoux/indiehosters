892747c770b5afc15d04c3630ecd17999e8c6855
Michiel de Jong <michiel@unhosted.org>
2014-10-06 12:22:34 +0000

    first attempt

--- scripts/adduser.sh
+++ scripts/adduser.sh
@@ -10,7 +10,16 @@ sleep 10
 IP=`docker inspect --format '{{.NetworkSettings.IPAddress}}' $2-$1`
 
 echo IP address of new container \'$2-$1\' is \'$IP\'
-sed s/%HOSTNAME%/$1/g /data/infrastructure/templates/haproxy-frontend.part | sed s/%IP%/$IP/g >> /data/server-wide/haproxy/frontends.part
+
+if [ -f /data/server-wide/haproxy/certs/%HOSTNAME%/combined.pem ] then
+sed s/%HOSTNAME%/$1/g /data/infrastructure/templates/haproxy-cert.part >> /data/server-wide/haproxy/certs.part
+else
+echo WARNING: TLS cert /data/server-wide/haproxy/certs/%HOSTNAME%/combined.pem not found! Not enabling SNI for this domain.
+fi
+
+sed s/%HOSTNAME%/$1/g /data/infrastructure/templates/haproxy-frontend.part >> /data/server-wide/haproxy/frontends.part
+
 sed s/%HOSTNAME%/$1/g /data/infrastructure/templates/haproxy-backend.part | sed s/%IP%/$IP/g >> /data/server-wide/haproxy/backends.part
-cat /data/server-wide/haproxy/haproxy-main.part /data/server-wide/haproxy/frontends.part /data/server-wide/haproxy/backends.part > /data/server-wide/haproxy/haproxy.cfg
+
+cat /data/server-wide/haproxy/haproxy-1.part /data/server-wide/haproxy/certs.part /data/server-wide/haproxy/haproxy-2.part /data/server-wide/haproxy/frontends.part /data/server-wide/haproxy/backends.part > /data/server-wide/haproxy/haproxy.cfg
 systemctl reload haproxy.service
