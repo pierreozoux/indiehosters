9c9886155422545378e193d4766a4cf79ba5402e
pierreozoux <pierre@ozoux.net>
2014-11-04 19:25:39 +0000

    Moves mysql to the new logic of runtime folder

--- importers/mysql.sh
+++ importers/mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-if [ ! -d "/data/domains/$DOMAIN/mysql/data" ]; then
-  mkdir -p /data/domains/$DOMAIN/mysql/data
+if [ ! -d "/data/domains/$DOMAIN/mysql/" ]; then
+  mkdir -p /data/domains/$DOMAIN/mysql/
   echo MYSQL_PASS=`echo $RANDOM  ${date} | md5sum | base64 | cut -c-10` > /data/domains/$DOMAIN/mysql/.env
 fi
