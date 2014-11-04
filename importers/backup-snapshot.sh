d19b7a084fd442d5bd44bff1d45012a97f90278a
Michiel de Jong <michiel@unhosted.org>
2014-11-04 14:49:04 +0000

    add wordpress-subdir image

--- importers/backup-snapshot.sh
+++ importers/backup-snapshot.sh
@@ -26,6 +26,18 @@ if [ -e /data/per-user/$USER/nginx ]; then
   fi
 fi
 
+if [ -e /data/per-user/$USER/wordpress-subdir ]; then
+  echo backing up www from wordpress-subdir for $USER
+  mkdir -p /data/per-user/$USER/backup/www/wordpress-subdir/
+  cp /data/per-user/$USER/wordpress-subdir/.env /data/per-user/$USER/backup/www/wordpress-subdir/.env
+  rsync -r /data/per-user/$USER/wordpress-subdir/data/wp-content /data/per-user/$USER/backup/www/wordpress-subdir/wp-content
+  if [ -e /data/per-user/$USER/wordpress-subdir/data/GITURL ]; then
+    cp /data/per-user/$USER/wordpress-subdir/data/GITURL /data/per-user/$USER/backup/www/wordpress-subdir/GITURL
+  else
+    rsync -r /data/per-user/$USER/wordpress-subdir/data/www-content /data/per-user/$USER/backup/www/wordpress-subdir/www-content
+  fi
+fi
+
 cd /data/per-user/$USER/backup/
 git add *
 git commit -m"backup $USER @ `hostname` - `date`"
