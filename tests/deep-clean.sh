226de322aaf5a771cc542e836c530cb1fb905ce6
Michiel de Jong <michiel@unhosted.org>
2015-01-04 18:52:46 +0100

    split in two

--- tests/deep-clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eux
-
-image=$1
-
-/data/indiehosters/tests/clean-image.sh $image
-
-ssh core@backup.dev "rm -rf $image.test"
--- tests/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eux
-
-applications=( static static-git wordpress known)
-
-for application in "${applications[@]}"
-do
-  /data/indiehosters/tests/image.sh $application
-done
